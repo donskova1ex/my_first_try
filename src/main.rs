d28e71f0cc46b23251ac06c3c96176adec80dca9
Alex Donskov <donskov_a@icloud.com>
2025-07-12 14:26:53 +0500

    add player asset

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use bevy::app::{App, Startup};
+use bevy::asset::AssetServer;
 use bevy::DefaultPlugins;
-use bevy::prelude::{Camera2dBundle, Commands};
+use bevy::prelude::{Camera2dBundle, Commands, Component, Res, Transform};
+use bevy::sprite::SpriteBundle;
+use bevy::utils::default;
+
+#[derive(Component)]
+struct Player;
 
 fn main() {
     App::new()
@@ -9,6 +15,17 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
+
+    let player_texture = asset_server.load("player.png");
+
+    commands.spawn((
+        SpriteBundle {
+            texture: player_texture,
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        },
+        Player,
+        ));
 }
